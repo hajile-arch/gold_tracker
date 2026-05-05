75762ee4cec02553d6fd2e44c9df1bdf5b3e3b0b
Hajile the Gonw <elijahwongwork@gmail.com>
2026-05-05 21:04:40 +0800

    Feat: Added a feature to get the time

--- pbe_selenium.py
+++ pbe_selenium.py
@@ -9,12 +9,12 @@ with webdriver.Chrome() as driver:
     
     sellingelem=driver.find_element(By.XPATH,"//td[contains(text(), '1 gram')]/following-sibling::td[1]").get_attribute("textContent").strip()
     buyingelem=driver.find_element(By.XPATH,"//td[contains(text(), '1 gram')]/following-sibling::td[2]").get_attribute("textContent").strip()
-    
+    time=driver.find_element(By.XPATH, "//*[contains(text(), 'Gold Investment Account as at')]").get_attribute("textContent").strip()
     # 2. Use "textContent" to grab the numbers even if the accordion is closed
     # We also add .strip() to remove any invisible extra spacing or line breaks
     # sell_price=sellingelem.get_attribute("textContent").strip()
     # buy_price=buyingelem.get_attribute("textContent").strip()
     
-    print(sellingelem + buyingelem)
+    print(buyingelem)
         
     
