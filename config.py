0183a36422b3d2cb5d5a08fcb8c474abb456a1ae
Hajile the Gonw <elijahwongwork@gmail.com>
2026-05-20 14:42:24 +0800

    Fix: Remove Cache_ttl and added RHB Bank Url

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ UOB_URL = "https://www.uob.com.my/wsm/stayinformed.do?path=gia"
 CIMB_URL = "https://www.cimb.com.my/en/personal/wealth-management/investments/investment-products/e-gold-investment-account-egia.html"
 MAY_URL = "https://www.maybank2u.com.my/maybank2u/malaysia/en/personal/rates/gold_and_silver.page"
 PBE_URL = "https://www.pbebank.com/en/invest/gold-egold-investment-account/"
+RHB_URL="https://www.rhbgroup.com/treasury-rates/precious-metal-exchange/index.html"
 
 HEADERS = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/145.0.0.0 Safari/537.36 Edg/145.0.0.0",
@@ -13,6 +14,6 @@ HEADERS = {
     "Referer": "https://www.google.com/"
 }
 
-CACHE_TTL = 900  # 15 minutes in seconds
+
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
